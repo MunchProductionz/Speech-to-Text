b0939027f6ed9047fd05d167f59f5daadccea27e
Henrik <henrrb@stud.ntnu.no>
2023-06-18 18:09:24 +0200

    Update API key

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,11 +1,12 @@
 import whisper
 import openai
 from languages import languages
+from api_keys import keys
 
 def speech_to_text(audio_filename, filetype="mp3", model="whisper-1", response_format="text", language="en"):
     
     # Set the OpenAI API key
-    openai.api_key = ""
+    openai.api_key = keys["OPENAI_API_KEY"]
     
     # Set the path to the audio file and get the language code
     audio_filepath = "audio/" + audio_filename + "." + filetype
