192d4ed48f5995225ff001b6af40351c27059fb8
Henrik <henrrb@stud.ntnu.no>
2023-06-18 18:10:54 +0200

    Add api_keys.py

---
+++ api_keys.py
@@ -0,0 +1,7 @@
+# Add your API keys here
+# You can get your OpenAI API key from https://beta.openai.com/account/api-keys
+# The key will not be pushed to GitHub when you commit your code
+
+keys = {
+    'OPENAI_API_KEY': "FILL IN YOUR OPENAI API KEY HERE",
+}
