fbe3c9f4ceaf3a116cd3cfb427d4b6191ec1b743
Henrik Bø <henrrb@stud.ntnu.no>
2026-05-07 23:34:52 +0200

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,7 +0,0 @@
-# Add your API keys here
-# You can get your OpenAI API key from https://beta.openai.com/account/api-keys
-# The key will not be pushed to GitHub when you commit your code
-
-keys = {
-    'OPENAI_API_KEY': "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",
-}
